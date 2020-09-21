db6c9efc0482d595925bb868a910bc0124296e45
Davidguzley <58478139+Davidguzley@users.noreply.github.com>
2020-09-21 10:41:29 -0500

    casos de prueba

--- Actividad Integral 1 .- Conceptos Básicos y Algoritmos Fundamentales/main.cpp
+++ Actividad Integral 1 .- Conceptos Básicos y Algoritmos Fundamentales/main.cpp
@@ -18,7 +18,7 @@ int main() {
 
   //Vector
 	vector<Cuenta> v;
-
+  
   //Variables del txt
   string nombre, apellidoP, apellidoM, cuentaNum;
   int dinero;
@@ -38,7 +38,8 @@ int main() {
 
     //Ordenamos y mostramos
     ordenaBurbuja(v);
-    //cout<< v.size()<<endl;
+    
+    //cout<< v.size()<<endl respuesta 10;
     for(int i = 0; i < v.size(); i++){
         v[i].mostrar();
     }
@@ -55,23 +56,46 @@ int main() {
     int r_inicio, r_final, inicio, final;
 
     cout << "\n" << "Define un rango de cantidad de dinero en cuentas para buscar" << endl;
+
     cout << "Rango inicial: " << endl;
     cin >> r_inicio;
-    //inicio = busqBinaria(v, 0);
-    //inicio = busqBinaria(v, 708);
-    //inicio = busqBinaria(v, 10);
     inicio = busqBinaria(v, r_inicio);
 
     cout << "Rango Final: " << endl;
     cin >> r_final;
-    //final= busqBinaria(v, 11);
-    //final= busqBinaria(v, 708);
-    //final= busqBinaria(v, 100000);
+
     final = busqBinaria(v, r_final);
 
     for (int i = inicio; i <= final; i++) {
         v[i].mostrar();
     }
+    //Casos de prueba
+    //Caso 1
+    //inicio = busqBinaria(v, 0);
+    //final= busqBinaria(v, 11);
+    //resultado: 00045678 David Guzman Leyva 0, 11132356 Emiliano Morales Landa 10
+    //Caso 2
+    //inicio = busqBinaria(v, 708);
+    //final= busqBinaria(v, 708);
+    //resultado: 01010101 Julio Gomez Gonzalez 708
+    //Caso 3
+    //inicio = busqBinaria(v, 10);
+    //final= busqBinaria(v, 1000000);
+    /*Resultados:
+    11132356 Emiliano Morales Landa 10
+    01010101 Julio Gomez Gonzalez 708
+    99343467 Jose Rodriguez Hernandez 47551
+    32148053 America Leyva Ramos 54341
+    21345667 Maria Lopez Gomez 65894
+    32532346 Armando Pablo Miranda 78123
+    52965034 Edgar Ramirez Robles 370567
+    85094325 Abigail Zapata Mendez 432950
+    70649583 Emilio Barrera Hernandez 1000000 */
+
+    //For para casos de prueba
+    /*for (int i = inicio; i <= final; i++) {
+        v[i].mostrar();
+    }*/
 	}
 
   else{
